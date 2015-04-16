68e6bad2268f2655bad70d30cbedfe0b9c97604a
nide <nenegoro@gmail.com>
2015-04-16 17:42:48 +0300

    Use csv package when extracting DB

--- extract_db.py
+++ extract_db.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import csv
 import sqlite3
+import sys
 from settings import *
 from optparse import OptionParser
 
+
+output = csv.writer(sys.stdout, delimiter=CSV_DELIMETER, quoting=CSV_QUOTING)
 
 parser = OptionParser()
 parser.add_option('-t', '--table', dest='table', help='DB table to be extracted')
@@ -15,14 +19,14 @@ db = sqlite3.connect(DB_NAME)
 cursor = db.cursor()
 
 cursor.execute('pragma table_info(%s)' % options.table)
-
-print CSV_DELIMETER.join(map(lambda C: C[1], cursor.fetchall()))
+output.writerow(map(lambda C: C[1], cursor.fetchall()))
 
 cursor.execute('select * from %s%s' % (
 	options.table,
 	options.orderby and ' order by %s' % options.orderby or ''
 ))
 
-print '\n'.join([CSV_DELIMETER.join([(F and str(F).strip()) or 'NULL' for F in P]) for P in cursor.fetchall()])
+for row in cursor.fetchall():
+	output.writerow([(C and str(C).strip()) or 'NULL' for C in row])
 
 db.close()
